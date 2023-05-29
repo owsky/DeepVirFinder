a9940a4b1d415d2fcf17887d1705339e88d446ec
Nicolò Bertocco <nick.bertocco@gmail.com>
2023-05-29 13:54:34 +0200

    Fixed scripts' running times

--- train_models.sh
+++ train_models.sh
@@ -1,18 +1,20 @@
-start=$(date +%s.%N)
+start_global=$(date +%s.%N)
 lengths=(150 300 500 1000)
+
 # Fragmenting sequences into fixed lengths, and encoding them using one-hot encoding (may take about 5 minutes)
+start_encoding=$(date +%s.%N)
 for l in "${lengths[@]}"
 do
   # for training
-  python encode.py -i ./train_data/tr/host_tr.fa -l "$l" -p host
-  python encode.py -i ./train_data/tr/virus_tr.fa -l "$l" -p virus
+  python encode.py -i ./train_example/tr/host_tr.fa -l "$l" -p host
+  python encode.py -i ./train_example/tr/virus_tr.fa -l "$l" -p virus
   # for validation
-  python encode.py -i ./train_data/val/host_val.fa -l "$l" -p host
-  python encode.py -i ./train_data/val/virus_val.fa -l "$l" -p virus
+  python encode.py -i ./train_example/val/host_val.fa -l "$l" -p host
+  python encode.py -i ./train_example/val/virus_val.fa -l "$l" -p virus
 done
 
 end=$(date +%s.%N)
-runtime=$(echo "$end - $start" | bc)
+runtime=$(echo "$end - $start_encoding" | bc)
 echo "Running time for encoding is $runtime"
 
 
@@ -21,14 +23,17 @@ echo "Running time for encoding is $runtime"
 # and 500 dense neurons in the dense layer. Training for 10 epochs.
 # Users may add THEANO_FLAGS='mode=FAST_RUN,device=cuda0,floatX=float32,GPUARRAY_CUDA_VERSION=80' in front of the python command to set GPU and cuda.
 # Using GPU (k40), the training process takes about 20 minutes
+# source /<path_to_cuda_setup>/setup.sh
+# source /<path_to_cuDNN_setup>/setup.sh
 for l in "${lengths[@]}"
 do
-  python training.py -l "$l" -i ./train_data/tr/encode -j ./train_data/val/encode -o ./models -f 10 -n 500 -d 500 -e 10
+  start_training=$(date +%s.%N)
+  THEANO_FLAGS='mode=FAST_RUN,device=cuda0,floatX=float32,GPUARRAY_CUDA_VERSION=80' python training.py -l "$l" -i ./train_example/tr/encode -j ./train_example/val/encode -o ./train_example/models -f 10 -n 500 -d 500 -e 10
   end=$(date +%s.%N)
-  runtime=$(echo "$end - $start" | bc)
+  runtime=$(echo "$end - $start_training" | bc)
   echo "Running time for training with length $l is $runtime"
 done
 
 end=$(date +%s.%N)
-runtime=$(echo "$end - $start" | bc)
+runtime=$(echo "$end - $start_global" | bc)
 echo "Global running time is $runtime"
