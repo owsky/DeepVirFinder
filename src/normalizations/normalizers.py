38920d40d8d8b20fc55dff8b886a9217f6f2fe0c
Nicolò Bertocco <nick.bertocco@gmail.com>
2023-07-13 13:10:11 +0200

    Computed z-count, removed quantile

--- src/normalizations/normalizers.py
+++ src/normalizations/normalizers.py
@@ -1,12 +1,10 @@
-from normalizations import count, divmax, log, mad, min_max, quantile, z_score, raw_count, z_score_count
+from normalizations import count, divmax, log, min_max, z_score, raw_count, z_score_count
 
 normalizers = {
     "count": count.normalize_sequence,
     "divmax": divmax.normalize_sequence,
     "log": log.normalize_sequence,
-    "mad": mad.normalize_sequence,
     "min_max": min_max.normalize_sequence,
-    "quantile": quantile.normalize_sequence,
     "z_score": z_score.normalize_sequence,
     "raw_count": raw_count.normalize_sequence,
     "z_score_count": z_score_count.normalize_sequence
